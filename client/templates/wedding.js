a34baf3c93a1577b3a93d01abba1d388f4d58ff5
parlihpb <parlihpb@gmail.com>
2016-04-01 15:13:35 -0400

    removing saving of names.

--- client/templates/wedding.js
+++ client/templates/wedding.js
@@ -2,21 +2,7 @@ RSVP = new Mongo.Collection("rsvp");
 
 if (Meteor.isClient) {
     Template.body.events({
-        "submit .rsvp": function (event, template) {
-            event.preventDefault();
-            var firstName = event.target.firstName.value;
-            var lastName = event.target.lastName.value;
-            var attending = event.target.attending.value;
 
-
-            RSVP.insert({
-                firstName: firstName,
-                lastName: lastName,
-                attending: attending,
-                createdAt: new Date()
-            });
-            template.find("form").reset();
-        }
     });
 }
 
